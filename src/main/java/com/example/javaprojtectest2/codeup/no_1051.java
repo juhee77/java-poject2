0e38a23f43c232db53bf840027ad282eb3de7f9d
juhee <bora2233@naver.com>
2023-04-27 11:10:39 +0900

    feat : code up 문제 49 ~ 52
    - code up 42 ~ 46

---
+++ src/main/java/com/example/javaprojtectest2/codeup/no_1051.java
@@ -0,0 +1,16 @@
+
+
+package com.example.javaprojtectest2.codeup;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class no_1051 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println((num1 <= num2) ? 1 : 0);
+    }
+}
