3cb4e59e2fdcff0cbf97dccc26a23f7d511788a3
juhee <bora2233@naver.com>
2023-05-09 15:54:14 +0900

    feat : 추상클래스, 생성자 오버로딩

---
+++ src/main/java/com/example/javaprojtectest2/week4/day2/ParallelogramShapeDrawer.java
@@ -0,0 +1,30 @@
+package com.example.javaprojtectest2.week4.day2;
+
+public class ParallelogramShapeDrawer extends ShapeDrawer {
+    private String spaceChar = "0";
+    private String fillStr = "*";
+    private int width = 4;
+
+    public ParallelogramShapeDrawer(String spaceChar, String fillStr, int width) {
+        this.spaceChar = spaceChar;
+        this.fillStr = fillStr;
+        this.width = width;
+    }
+
+    public ParallelogramShapeDrawer(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
+
+    public ParallelogramShapeDrawer() {
+    }
+
+    @Override
+    public String makeALine(int height, int index) {
+        return spaceChar.repeat(height - index - 1) + fillStr.repeat(width) +" \n";
+    }
+
+    @Override
+    public String makeFullLine(int height, int index) {
+        return spaceChar.repeat(height - index - 1) + fillStr.repeat(width) +spaceChar.repeat(index)+" \n";
+    }
+}
