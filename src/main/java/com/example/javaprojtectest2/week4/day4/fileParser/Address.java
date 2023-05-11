483e33c5c7bc24fa8075bcdc5300c28975f50196
juhee <bora2233@naver.com>
2023-05-11 16:04:09 +0900

    feat : file input

---
+++ src/main/java/com/example/javaprojtectest2/week4/day4/fileParser/Address.java
@@ -0,0 +1,5 @@
+package com.example.javaprojtectest2.week4.day4.fileParser;
+
+public class Address {
+
+}
---
+++ src/main/java/com/example/javaprojtectest2/week4/day4/fileParser/Hospital.java
@@ -0,0 +1,13 @@
+package com.example.javaprojtectest2.week4.day4.fileParser;
+
+public class Hospital {
+    private String name;
+    private String phoneNumber;
+    private Address address;
+
+    public Hospital(String name, String phoneNumber, Address address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
